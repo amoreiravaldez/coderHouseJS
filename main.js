d5d1e63b4067956a7aec686f570955d5ad3c3f83
Alvaro Moreira <amoreiravaldez@gmail.com>
2022-01-06 20:38:50 -0300

    Cambios en estructura de archios

--- main.js
+++ main.js
@@ -63,11 +63,3 @@ else if (isNaN(year) == false) {
  
     }
 }
-
-// Año bisiesto:
-
-// 1. Si el año es uniformemente divisible por 4, vaya al paso 2. De lo contrario, vaya al paso 5.
-// 2. Si el año es uniformemente divisible por 100, vaya al paso 3. De lo contrario, vaya al paso 4.
-// 3. Si el año es uniformemente divisible por 400, vaya al paso 4. De lo contrario, vaya al paso 5.
-// 4. El año es un año bisiesto (tiene 366 días).
-// 5. El año no es un año bisiesto (tiene 365 días).
