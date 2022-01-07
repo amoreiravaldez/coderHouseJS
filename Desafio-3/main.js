538681a4a1f25c98f8e36e97b711be18f5877367
Alvaro Moreira <amoreiravaldez@gmail.com>
2022-01-07 15:48:19 -0300

    Update final del desafío 3

--- Desafio-3/main.js
+++ Desafio-3/main.js
@@ -1,6 +1,27 @@
-function saludar () {
+// Declaro la variable termo de carácter numérico que contiene un valor que posteriormente voy a reducir hasta llegar a cero y salir del bucle.
 
-    console.log("Hola")
+let termo = 5; 
+
+while (termo > 0) {
+
+    if (termo > 1) {
+        console.log("Toma, te paso un mate!");
+        console.log("( ^-^)_旦”");
+
+        console.log("Gracias, acá te lo devuelvo!");
+        console.log("”旦_(^-^)")
+    } else {
+        console.warn("Toma, te paso un mate, aunque creo que es el último.");
+        console.warn("( ^-^)_旦”");
+
+        console.log("Gracias, acá te lo devuelvo!");
+        console.log("”旦_(^-^)")
+    }
+    
+    termo -= 1;
 }
 
-saludar();
+// Una vez que el bucle llega a cero, salgo y muestro el mensaje final por consola.
+
+console.error("Nos quedamos sin agua! Ahí voy a cargar más.")
+console.error("(o⌓o)....")
