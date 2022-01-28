e628df51b293e394798dea5f504086e7ed57943a
Alvaro Moreira <amoreiravaldez@gmail.com>
2022-01-27 23:43:29 -0300

    Desafío 8

---
+++ Desafío-8/js/handler.js
@@ -0,0 +1,21 @@
+// Busco todos los botones del DOM que hagan referencia a la clase .btnAddNewItem y loopeo entre ellos
+// con un forEach para añadir un event listener a cada uno.
+
+let btnAddNewItem = document.querySelectorAll('.btnAddNewItem').forEach(item => {
+    item.addEventListener('click', () => {
+        
+        const newWorker = newWorkerDetails(item.id);
+
+        if (isNaN(newWorker.title) || isNaN(newWorker.cost) || isNaN(newWorker.quantity) || newWorker.title < 1 || newWorker.cost < 1 || newWorker.quantity < 1) {
+
+            alert("El costo por hora o la cantidad de horas son incorrectas. Por favor ingrese un número válido para el nuevo item.");
+            return;
+    
+        } else {
+
+            updateResults(newWorker);
+            workersDistribution(newWorker);
+        
+        }
+    })
+});
